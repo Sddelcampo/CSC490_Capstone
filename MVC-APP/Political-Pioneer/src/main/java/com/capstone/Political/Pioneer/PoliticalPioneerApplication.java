c77947c73f78d1a00c48847fe1aa77458d532396
James Weah <122507491+jweah555@users.noreply.github.com>
2026-09-02 14:48:26 -0400

    Springboot initializer folder

---
+++ MVC-APP/Political-Pioneer/src/main/java/com/capstone/Political/Pioneer/PoliticalPioneerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.Political.Pioneer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalPioneerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalPioneerApplication.class, args);
+	}
+
+}
---
+++ MVC-APP/Political-Pioneer/src/test/java/com/capstone/Political/Pioneer/PoliticalPioneerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.Political.Pioneer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticalPioneerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
