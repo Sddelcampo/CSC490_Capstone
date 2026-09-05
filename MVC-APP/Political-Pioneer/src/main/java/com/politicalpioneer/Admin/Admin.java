4f21ef999729b021aa985b1c0b3c35fae1988657
James Weah <122507491+jweah555@users.noreply.github.com>
2026-09-05 10:31:44 -0400

    Created party member and admin schema

---
+++ MVC-APP/Political-Pioneer/src/main/java/com/politicalpioneer/Admin/Admin.java
@@ -0,0 +1,94 @@
+package com.politicalpioneer.Admin;
+
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long adminId;
+
+    @Column(name = "fist_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Admin() {}
+
+    public Admin(Long adminId, String firstName, String lastName, String userName, String password) {
+        this.adminId = adminId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = userName;
+    }
+
+
+    public Long getAdminId() {
+        return this.adminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+     public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
+
+    
+
+    
+    
+}
---
+++ MVC-APP/Political-Pioneer/src/main/java/com/politicalpioneer/PartyMember/PartyMember.java
@@ -0,0 +1,135 @@
+package com.politicalpioneer.PartyMember;
+import java.io.DataOutput;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.springframework.data.auditing.DateTimeProvider;
+
+import com.politicalpioneer.Party.Party;
+import com.politicalpioneer.User.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import tools.jackson.databind.cfg.DateTimeFeature;
+
+//Creates composite primary key for partymembers
+@Embeddable
+class PartyMemberId implements Serializable {
+    private Long userId;
+    private Long partyId;
+
+    public PartyMemberId(Long userId, Long partyId ) {
+        this.userId = userId;
+        this.partyId = partyId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof PartyMemberId)) return false;
+        PartyMemberId that = (PartyMemberId) o;
+        return Objects.equals(userId, that.userId)
+            && Objects.equals(partyId, that.partyId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, partyId);
+    }
+}
+
+
+
+@Entity
+@Table(name = "party_member")
+public class PartyMember {
+    //References PartyMemberId class
+    @Id
+    private PartyMemberId id;
+
+    //Passes / Maps User and Party Id to the composite id
+    //of the partymember class
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @MapsId("partyId")
+    @JoinColumn(name = "party_id")
+    private Party party;
+
+    @Column(name="date_join", nullable = false)
+    private LocalDateTime joinDate;
+
+    @Column
+    private String status;
+
+    public PartyMember(PartyMemberId id, User user, Party party, LocalDateTime joinDate, String status) {
+        this.id = id;
+        this.user = user;
+        this.party = party;
+        this.joinDate = joinDate;
+        this.status = status;
+    }
+
+    public PartyMemberId getId() {
+        return id;
+    }
+
+    public void setId(PartyMemberId id) {
+        this.id = id;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+     public Party getParty() {
+        return party;
+    }
+
+    public void setParty(Party party) {
+        this.party = party;
+    }
+
+    public LocalDateTime getDate() {
+        return joinDate;
+    }
+
+    public void setDate(LocalDateTime joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+
+
+
+
+
+
+}
